98d6e441ce67feeaf914afdc80a39a02ce9fdc14
derikbf <derikb@gmail.com>
2022-03-29 20:02:39 -0300

    Updated shared

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
       resources :exams
       resources :treatments
       resources :user_shares
+      scope :shares do
+        get "medical_appointments", to: "shares#medical_appointments"
+        get "exams", to: "shares#exams"
+        get "treatments", to: "shares#treatments"
+      end
     end
   end
 end
