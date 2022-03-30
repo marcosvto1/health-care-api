5ec9287a8d6f82d55727d4a0badc14d2ac0fe250
derikbf <derikb@gmail.com>
2022-03-30 18:04:01 -0300

    Criado o Reset Password

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for "User", at: "auth"
+    
   namespace "api" do
     namespace "v1" do
+
       resources :medical_appointments
       resources :exams
       resources :treatments
@@ -11,6 +13,10 @@ Rails.application.routes.draw do
         get "exams", to: "shares#exams"
         get "treatments", to: "shares#treatments"
       end
+
+      post 'password/forgot', to: 'passwords#forgot'
+      post 'password/reset', to: 'passwords#reset'
+      
     end
   end
 end
