a4d6a8a1dd87afaec936894d297143a3c7097092
Marcos <marcosvto1@gmail.com>
2022-04-11 20:24:33 -0300

    ability params divise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :configure_permitted_parameters
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :email, :password, :kind, :cell_phone, :crm])
+  end
 end
